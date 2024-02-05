d078ddc361f7c3585345d0c83f32257dc6835725
Cedric Kienzler <github@cedric-kienzler.de>
2024-02-05 15:51:20 +0100

    update sops secrets

--- hack/encrypt-secrets.sh
+++ hack/encrypt-secrets.sh
@@ -14,7 +14,8 @@ function join_by {
     fi;
 }
 
-age_keys=('age1r9chn8pl3d4msxktw457x3xz2l8p04pwuyd7pkgldkmkakras5ks7tfsyq') # cedi
-age_keys+=('age10hzpv26wext204acftvf2n8r6hmnnwxdpkngzaysllpwynccjvjsjvhq4z') # k8s_cedi_dev.txt
+age_keys=('age1r9chn8pl3d4msxktw457x3xz2l8p04pwuyd7pkgldkmkakras5ks7tfsyq') # cedi@mae
+age_keys+=('age10hzpv26wext204acftvf2n8r6hmnnwxdpkngzaysllpwynccjvjsjvhq4z') # argocd (k8s_cedi_dev_new)
+age_keys+=('age1l4lhmkmvr4lype9x4dm3y9lxk2uekqwwj65mp4c36tnph6ytfakqesnats') # GH Codespaces (keys_ghcodespace)
 
 find . -name "*.secret.yaml" | xargs -n1 sops --age $(join_by , "${age_keys[@]}") -e -i
