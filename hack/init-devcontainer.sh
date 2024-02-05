b66baa11e55d5b43fe9f2e6a45640d93db455b09
Cedric Kienzler <github@cedric-kienzler.de>
2024-02-05 16:12:39 +0100

    update codespace config

---
+++ hack/init-devcontainer.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Setup kubeconfig
+mkdir -p ~/.kube/
+echo "${DEV_KUBECONFIG}" > ~/.kube/config
+[[ -d "$HOME/.kube" && -f "$HOME/.kube/config" ]] && export $KUBECONFIG=$HOME/.kube/config
+
+# Setup SOPS AGE Keys
+mkdir -p ~/.age/sops/
+echo "${SOPS_AGE_KEYS}" > ~/.age/sops/keys.txt
+[[ -d "$HOME/.age/sops" && -f "$HOME/.age/sops/keys.txt" ]] && export SOPS_AGE_KEY_FILE="$HOME/.age/sops/keys.txt"
